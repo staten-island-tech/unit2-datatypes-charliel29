628e183da702f07aaa91761837306da53dcb1e8d
charliel29 <charliel29@nycstudents.net>
2025-09-17 09:26:23 -0400

    numbers

---
+++ notes.py
@@ -0,0 +1,25 @@
+""" friend_comes=True
+money=True
+
+def and_movies(friend,money):
+         if friend == True and money == True:
+          print("Going to the movies")
+         else:
+            print("I have no friends or i have no money")
+        
+""" """
+
+and_movies(friend_comes,money) """
+""" 
+homework=True
+def not_movies(homework):
+    if not homework:
+        print("movie time")
+    else:
+        print("hommework time,I hate russian")
+not_movies(homework)  """
+
+def factor(x,y):
+    if x % y == 0:
+        print(f"{y}is a factor of {x}")
+factor(25,5)
---
+++ test.py
@@ -0,0 +1,3 @@
+def oddoreven(x,y):
+    if x % y==0:
+        print(f"{x}")
