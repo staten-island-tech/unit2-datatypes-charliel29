61ce50b504c1e137e7147d248f04cc219dd11b4a
charliel29 <charliel29@nycstudents.net>
2025-09-19 09:24:27 -0400

    bill 2 delete print

--- test.py
+++ test.py
@@ -1,6 +1,21 @@
-bill = 12.99
-service = bad
+service = 0
 def tipcalc():
-    tip = "how was service? Bad , okay , Good , Great"
-    if service == "bad":
-        
+    tip = input ("How was the service? Bad, Okay, Good, Great")
+    tip_perc = 0    
+    if tip == "Great":
+      tip_perc = 0.25
+    elif tip == "Good":
+      tip_perc = 0.20
+    elif tip == "Okay":
+      tip_perc = 0.15
+    elif tip == "bad":
+      tip_perc = 0
+    else:
+        print ("Invalid")
+    bill = float(input("what is your billamount"))
+    print(tip_perc)
+    subtotal = tip_perc * bill
+    print(subtotal)
+    Total = subtotal + bill 
+    print (f"This is your total: {Total}")
+tipcalc()
